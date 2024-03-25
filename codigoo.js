3831210a1f38c9ebc2d0f74f91c06308a6302733
RAMIRO SUED <47958668@ort>
2024-03-25 11:42:21 -0300

    v2

--- codigoo.js
+++ codigoo.js
@@ -9,7 +9,6 @@ let tareas=[
 {nombre:"Tarea de Efsi", completada:false},
 {nombre:"Tarea de quimica", completada:false}
 ]
-let html =""
 /*tareas.forEach(n=> {
    
 html += `<li>${n.nombre}</li>`;
@@ -17,55 +16,34 @@ html += `<li>${n.nombre}</li>`;
   actualizarListaTareas();
 }
 )*/
-    tareas.forEach(n => {
+function mostrarTareas(){
+    let html =""
+    tareas.forEach((n,index) => {
         if (n.completada==true) {
-            html += `<li style="text-decoration: line-through;">${n.nombre}</li>`;
+            html += `<li onclick="tacharTarea(${index})" style="text-decoration: line-through;">${n.nombre}</li>`;
         } else {
-            html += `<li><button id="boton">${n.nombre}</button></li>`;
+            html += `<li><button onclick="tacharTarea(${index})">${n.nombre}</button></li>`;
         }
     });
 listaTareas.innerHTML=html
+}
+mostrarTareas()
 
-listaTareas.onclick = function(){
-     tareas[3].completada=true
-     html=""
+ function tacharTarea(i){
+    if(tareas[i].completada==true){
+        tareas[i].completada=false;
+    }else tareas[i].completada=true;
     console.log("hika")
-    tareas.forEach(n => {
-        if (n.completada==true) {
-            html += `<li style="text-decoration: line-through;">${n.nombre}</li>`;
-        } else {
-            html += `<li><button id="boton">${n.nombre}</button></li>`;
-        }
-    })
-    listaTareas.innerHTML=html
-
+    mostrarTareas()
 }
 
-/*
-let elementosTareas = listaTareas.querySelectorAll('li');
-    elementosTareas.forEach((elemento, index) => {
-        elemento.onclick = function() {
-            marcarComoCompletada(index);
-        };  
-    });
-    function marcarComoCompletada(index) {
-        tareas.completada = true;
-        tareas.forEach(n => {
-            if (n.completada==true) {
-                html += `<li style="text-decoration: line-through;">${n.nombre}</li>`;
-            } else {
-                html += `<li><button>${n.nombre}</button></li>`;
-            }
-        });}
 
-*/
 console.log(tareas)
 
 document.getElementById("agregar").onclick =()=>{
     if(nueva.value.length > 0){
         tareas.push({nombre:nueva.value, completada:false })
-        html +=`<li><button>${nueva.value}</button></li>`;
-        listaTareas.innerHTML=html
+        mostrarTareas()   
         console.log(tareas)
         
     }else alert('ingrese una tarea')
